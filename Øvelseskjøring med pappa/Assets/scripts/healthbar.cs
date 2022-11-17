b047848cc98f33a91b594f38494c31d71b5687a5
vbw2 <halvbart@gmail.com>
2022-11-17 09:43:00 +0100

    piss

---
+++ Øvelseskjøring med pappa/Assets/scripts/healthbar.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthbar : MonoBehaviour
+{
+    public Image angry;
+
+        public Sprite en;
+        public Sprite to;
+        public Sprite tre;
+        public Sprite fire;
+        public Sprite fem;
+        public Sprite seks;
+        public Sprite syv;
+        public Sprite åtte;
+        public Sprite ni;
+        public Sprite ti;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
