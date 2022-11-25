970af2b47d85375d090d75dec6f0fff0b7d7cb9e
vbw2 <halvbart@gmail.com>
2022-11-25 13:51:55 +0100

    yes

--- Øvelseskjøring med pappa/Assets/scripts/spriteChange.cs
+++ Øvelseskjøring med pappa/Assets/scripts/spriteChange.cs
@@ -13,6 +13,15 @@ public class spriteChange : MonoBehaviour
     private GameObject treff;
      public healthbar treffScript;
 
+
+
+    //Einar her
+
+    public GameObject bar0;
+    public GameObject bar1;
+    public GameObject bar2;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +36,28 @@ public class spriteChange : MonoBehaviour
     void Update()
     {
         //if (public int Number= 2)
+        hits = treffScript.Number;
+
+        if (hits == 0)
+        {
+            bar0.SetActive(true);
+        }
+        else
+        {
+            bar0.SetActive(false);
+        }
+        if (hits == 1)
+        {
+            bar1.SetActive(true);
+        }
+        else bar1.SetActive(false);
+        if (hits == 2)
+        {
+            bar2.SetActive(true);
+        }
+        else bar2.SetActive(false);
+
+
     }
 
 
