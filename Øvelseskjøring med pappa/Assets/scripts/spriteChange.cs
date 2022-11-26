4ab70a2c39204f54dbcc672f1980dac9c913b637
vbw2 <halvbart@gmail.com>
2022-11-26 11:24:34 +0100

    yes ett eller annet ferdig

--- Øvelseskjøring med pappa/Assets/scripts/spriteChange.cs
+++ Øvelseskjøring med pappa/Assets/scripts/spriteChange.cs
@@ -4,38 +4,17 @@ using UnityEngine;
 
 public class spriteChange : MonoBehaviour
 {
-    // har med tall å gjøre eksempel health bar eller damage
-    public int hits = 0;
-
-    // bool fordi det er en ja eller nei statement
-    public bool visible=true;
-    public spriteChange dummythick;
-    private GameObject treff;
-     public healthbar treffScript;
-
-
-
-    //Einar her
+    public healthbar treffScript;
 
     public GameObject bar0;
     public GameObject bar1;
     public GameObject bar2;
 
+    // har med tall å gjøre eksempel health bar eller damage
+    public int hits = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //treff = GameObject.FindGameObjectWithTag("GameObject");
-        //treffScript = healthbar.GetComponent < public void OnCollisionEnter2D(Collision2D collision)>;
-        //treffScript = treff.GetComponent<healthbar>();
-
-       // treffScript.OnCollisionEnter2D(Collision2D collision);
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        //if (public int Number= 2)
         hits = treffScript.Number;
 
         if (hits == 0)
@@ -46,11 +25,13 @@ public class spriteChange : MonoBehaviour
         {
             bar0.SetActive(false);
         }
+        
         if (hits == 1)
         {
             bar1.SetActive(true);
         }
         else bar1.SetActive(false);
+        
         if (hits == 2)
         {
             bar2.SetActive(true);
